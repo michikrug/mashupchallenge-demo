ec7c117e5a926bd69397c94139fd8dd7b49f949b
Michael Krug <krumi@hrz.tu-chemnitz.de>
2016-05-25 13:17:56 +0200

    minor change

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-var finalhandler = require('finalhandler'),
-	http = require('http'),
-	serveStatic = require('serve-static');
-
-var serve = serveStatic('.', {'index': ['index.html']});
-
-var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res);
-  serve(req, res, done);
+var serves = require('serve-static')('.', {'index': ['index.html']});
+var server = require('http').createServer(function(req, res){
+  serves(req, res, require('finalhandler')(req, res));
 })
-
 server.listen(80);
